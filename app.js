17a988a8875ab8373a0e62983caa47f290999774
Antoine RICHARD <antoine.richard.partner@decathlon.com>
2023-02-06 21:03:56 +0100

    refactor(prettier): format files

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-const { getArguments } = require('./lib/command');
-const { filterByAnimal, countEntities } = require('./lib/data');
-const { data: initialData } = require('./data');
+const { getArguments } = require("./lib/command");
+const { filterByAnimal, countEntities } = require("./lib/data");
+const { data: initialData } = require("./data");
 
 const arguments = getArguments();
 
 let data = initialData;
 
 if (arguments.help) {
-    console.log('Usage : node app.js [--filter=pattern] [--count]');
-    process.exit(0);
+  console.log("Usage : node app.js [--filter=pattern] [--count]");
+  process.exit(0);
 }
 
 if (arguments.filter) data = filterByAnimal(data, arguments.filter);
